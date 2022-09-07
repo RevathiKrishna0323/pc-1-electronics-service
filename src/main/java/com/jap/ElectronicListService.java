fa0450bb7cd89e694b50d4aa2c80f39304d62b5a
Revathi G <revathikrishna0323@gmail.com>
2022-09-07 16:46:14 +0530

    added logic to searchElectronicItemInList

--- src/main/java/com/jap/ElectronicListService.java
+++ src/main/java/com/jap/ElectronicListService.java
@@ -17,10 +17,13 @@ public class ElectronicListService {
 
     //search the electronic item from the list and return the index position
     public static int searchElectronicItemInList(List<String> itemList, String searchItem) {
-
-            return -1;
+        List<String> list = new ArrayList<>();
+        int index = itemList.indexOf("Refrigerator");
+        if (index != -1) {
+            System.out.println(index);
         }
-
+        return index;
+    }
 
     //remove the electronic item from the list using iterator
     public static boolean removeElectronicsItemFromList(List<String> itemList, String removeItem)
